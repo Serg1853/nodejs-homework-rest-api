b51c5651190cd9fb9dc9f3040b184e1334c8c789
Serg1853 <sergey24226@gmail.com>
2024-01-14 18:10:21 +0200

    token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -32,7 +36,10 @@ const login = async (req, res) => {
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
   }
-  const token = "skdfhgsjhfsfgs.skdufhksufhsf.ksdfhsdfh";
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
   res.json({ token });
 };
 
